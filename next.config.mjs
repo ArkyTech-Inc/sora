d9a12faa4ceb9554065a7bbff56f3389915681f0
Arky <motolanisomoye@gmail.com>
2026-08-16 00:44:45 +0100

    added changes to make it a PWA, utilized Serwist

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,14 @@
+import withSerwistInit from "@serwist/next";
+
+const withSerwist = withSerwistInit({
+  // Points to your custom service worker file
+  swSrc: "app/sw.ts",
+  // The output file that the browser will load
+  swDest: "public/sw.js",
+  // Disable in dev mode to prevent caching confusion while coding
+  disable: process.env.NODE_ENV === "development",
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
@@ -6,6 +17,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-}
+};
 
-export default nextConfig
+export default withSerwist(nextConfig);
